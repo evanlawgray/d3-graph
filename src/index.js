87a5d6b3393353684af5c6a8d17edcb7da2ccf51
Evan Gray <evanlawgray@gmail.com>
2017-11-26 15:04:30 -0800

    Make bar widths dynamic; hide document until js bundle load to avoid FOUC.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 require('./sass/styles.scss');
 const d3 = require('d3');
 
+window.addEventListener('load', () => {
+  // Avoid flicker of unstyled content on page load
+  document.body.classList.remove( 'loading' );
 
+  const canvasWidth = document.getElementById('bar-graph').offsetWidth;
+  let barWidth;
 
-const data = [15, 33, 12, 49, 188, 99];
+  const data = [15, 33, 12, 49, 188, 99];
 
+  const tooltips = d3.selectAll( '.tooltips' )
 
-d3.select( '#bar-graph' )
-  .selectAll( 'div' )
-  .data( data )
-  .enter()
-  .append( 'div' )
-    .attr( 'class', 'bar' )
-    .style('height', (d, i) => d * 2 + "px"  )
-  .append( 'p' )
-    .attr( 'class', 'tooltip' )
-    .text( (d) => `${d}` );
+  d3.select( '#bar-graph' )
+    .selectAll( 'div' )
+    .data( data )
+    .enter()
+    .append( 'div' )
+      .attr( 'class', 'bar' )
+      .style( 'height', (d) => d * 2 + "px" )
+      .style( 'width', () => {
+        barWidth = Math.min( ( canvasWidth / data.length ) - 60, 100 );
+        return barWidth + "px";
+      })
+    .append( 'p' )
+      .attr( 'class', 'tooltip' )
+      .text( (d) => `${d}` )
+      .style( 'left', () => {
+        return ( barWidth - 50 ) / 2 + "px";
+      })
+});
